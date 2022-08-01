d32dd4009c810bee628dbfa2d98e008fb7e6f9e2
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-08-01 19:03:34 +0900

    add Fill code

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,45 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@R0
+        M=0
+        @R1
+        M=0
+(LOOP)  
+        @KBD
+        D=M
+        @COLORBLACK
+        D;JEQ
+        @R0    //color
+        M=0
+        @PAINT
+        0;JMP
+(COLORBLACK)
+        @R0    //color
+        M=-1
+        @PAINT
+        0;JMP
+(PAINT)
+        @SCREEN
+        D=A
+        @8191
+        D=D+A
+        @R1  // Address 
+        M=D
+(FILL)
+        @R0  // color
+        D=M
+        @R1
+        A=M
+        M=D
+        @R1
+        MD=M-1
+        @SCREEN
+        D=D-A
+        @FILL
+        D;JGE
+        @LOOP
+        0;JMP
+(END)
+        @END
+        0;JMP
